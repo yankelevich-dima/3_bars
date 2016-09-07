d9fc5fb9d5ef021427aa793974a25b54315692fb
Dmitry <yankelevich.dima@gmail.com>
2016-09-07 11:52:29 +0300

    Added bars.py

--- bars.py
+++ bars.py
@@ -2,20 +2,33 @@ import json
 
 
 def load_data(filepath):
-    pass
+    data = ''
+    with open(filepath, 'r') as infile:
+        data = infile.read()
+    return json.loads(data)
 
 
 def get_biggest_bar(data):
-    pass
+    return max(data, key=lambda x: x['Cells']['SeatsCount'])
 
 
 def get_smallest_bar(data):
-    pass
+    return min(data, key=lambda x: x['Cells']['SeatsCount'])
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+
+    def get_distance(bar):
+        lon, lat = bar['Cells']['geoData']['coordinates']
+        return ((lon - longitude) ** 2 + (lat - latitude) ** 2) ** 0.5
+
+    return min(data, key=get_distance)
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('bars.json')
+    print(get_biggest_bar(data))
+    print(get_smallest_bar(data))
+    lon = float(input('Enter longtitude: '))
+    lat = float(input('Enter latitude: '))
+    print(get_closest_bar(data, lon, lat))
